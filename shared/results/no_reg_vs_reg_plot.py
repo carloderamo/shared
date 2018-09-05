9157b22554e10550c19a234a1d166bd5e41341ed
carloderamo <carlo.deramo@gmail.com>
2018-09-05 16:52:51 +0200

    fix in plotter

--- shared/results/no_reg_vs_reg_plot.py
+++ shared/results/no_reg_vs_reg_plot.py
@@ -11,37 +11,38 @@ def get_mean_and_confidence(data):
     interval, _ = st.t.interval(0.95, n-1, scale=se)
 
     return mean, interval
-
+    
 games = ['cart.npy', 'acro.npy', 'mc.npy', 'coh.npy', 'pend.npy']
 n_games = len(games)
 
-folders = ['multi', 'multireg1e-4', 'multireg1e-4sigmoid']
+multi = np.load('multireg1e-4/multi.npy')
+multi_reg = np.load('multireg1e-4sigmoid/multi.npy')
 
 plt.suptitle('NO REG VS REG')
 
-for j, f in enumerate(folders):
-    for i, g in enumerate(games):
-        if g == '':
-            continue
+for i, g in enumerate(games):
+    if g == '':
+        continue
 
-        plt.subplot(n_games, 1, i + 1)
-        plt.title(g)
+    plt.subplot(n_games, 1, i + 1)
+    plt.title(g)
 
-        multi_game = multi[:, i]
-        multi_game_mean, multi_game_err = get_mean_and_confidence(multi_game)
-        
-        plt.plot(multi_game_mean)
-        plt.fill_between(np.arange(51), multi_game_mean - multi_game_err, multi_game_mean + multi_game_err, alpha=.5)
-        
-        multi_reg_game = multi_reg[:, i]
-        multi_reg_game_mean, multi_reg_game_err = get_mean_and_confidence(multi_reg_game)
-        
-        plt.plot(multi_reg_game_mean)
-        plt.fill_between(np.arange(51), multi_reg_game_mean - multi_reg_game_err, multi_reg_game_mean + multi_reg_game_err, alpha=.5)
-        
+    multi_game = multi[:, i]
+    multi_game_mean, multi_game_err = get_mean_and_confidence(multi_game)
+    
+    plt.plot(multi_game_mean)
+    plt.fill_between(np.arange(51), multi_game_mean - multi_game_err, multi_game_mean + multi_game_err, alpha=.5)
+    
+    multi_reg_game = multi_reg[:, i]
+    multi_reg_game_mean, multi_reg_game_err = get_mean_and_confidence(multi_reg_game)
+    
+    plt.plot(multi_reg_game_mean)
+    plt.fill_between(np.arange(51), multi_reg_game_mean - multi_reg_game_err, multi_reg_game_mean + multi_reg_game_err, alpha=.5)
+    
     plt.grid()
     
-plt.legend(['NO REG', 'REG', 'REG_SIGMOID'])
+plt.legend(['NO REG', 'REG'])
     
 plt.show()
     
+
