701a9bc45c082664e04bc555ff7fc299e2a23b01
carloderamo <carloderamo@gmail.com>
2018-09-06 17:42:01 +0200

    atari run updated

--- shared/run_atari.py
+++ shared/run_atari.py
@@ -9,6 +9,8 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 
+import pickle
+
 sys.path.append('..')
 
 from mushroom.approximators.parametric import PyTorchApproximator
@@ -17,7 +19,7 @@ from mushroom.utils.dataset import compute_scores
 from mushroom.utils.parameters import LinearDecayParameter, Parameter
 
 from core import Core
-from shared.dqn import DQN
+from shared.dqn import DQN, DoubleDQN
 from policy import EpsGreedyMultiple
 
 """
@@ -28,7 +30,8 @@ This script runs Atari experiments with DQN as presented in:
 
 
 class Network(nn.Module):
-    def __init__(self, input_shape, _, n_actions_per_head, use_cuda, dropout):
+    def __init__(self, input_shape, _, n_actions_per_head, use_cuda, dropout,
+                 features):
         super(Network, self).__init__()
 
         n_input = input_shape[0]
@@ -37,51 +40,54 @@ class Network(nn.Module):
         self._use_cuda = use_cuda
         self._dropout = dropout
         self._n_shared = 6
+        self._features = features
 
-        self._h1 = nn.Conv2d(n_input, 32, kernel_size=8, stride=4)
+        self._h1 = nn.ModuleList([nn.Conv2d(
+            n_input, 32, kernel_size=8, stride=4) for _ in range(self._n_games)]
+        )
         self._h2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
         self._h3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
-        self._h4 = nn.ModuleList([nn.Linear(3136, 512) for _ in range(
-            self._n_games)])
+        self._h4 = nn.Linear(3136, 512)
         self._h5 = nn.ModuleList(
             [nn.Linear(512, self._max_actions) for _ in range(
                 self._n_games)])
 
         if self._dropout:
-            self._h1_dropout = nn.Dropout2d()
-            self._h2_dropout = nn.Dropout2d()
-            self._h3_dropout = nn.Dropout2d()
+            raise NotImplementedError
 
-        nn.init.xavier_uniform_(self._h1.weight,
-                                gain=nn.init.calculate_gain('relu'))
         nn.init.xavier_uniform_(self._h2.weight,
                                 gain=nn.init.calculate_gain('relu'))
         nn.init.xavier_uniform_(self._h3.weight,
                                 gain=nn.init.calculate_gain('relu'))
+        nn.init.xavier_uniform_(self._h4.weight,
+                                gain=nn.init.calculate_gain('relu'))
         for i in range(self._n_games):
-            nn.init.xavier_uniform_(self._h4[i].weight,
+            nn.init.xavier_uniform_(self._h1[i].weight,
                                     gain=nn.init.calculate_gain('relu'))
             nn.init.xavier_uniform_(self._h5[i].weight,
                                     gain=nn.init.calculate_gain('linear'))
 
     def forward(self, state, action=None, idx=None, get_features=False):
-        h = F.relu(self._h1(state.float() / 255.))
-        if self._dropout:
-            h = F.relu(self._h2(self._h1_dropout(h)))
-            h = F.relu(self._h3(self._h2_dropout(h)))
-            h_f = self._h3_dropout(h).view(-1, 3136)
+        state = state.float() / 255.
+
+        h1 = list()
+        for i in np.unique(idx):
+            idxs = np.argwhere(idx == i).ravel()
+            h1.append(F.relu(self._h1[i](state[idxs])))
+        cat_h1 = torch.cat(h1)
+
+        h = F.relu(self._h2(cat_h1))
+        h = F.relu(self._h3(h))
+        h_f = h.view(-1, 3136)
+
+        if self._features == 'relu':
+            h_f = F.relu(self._h4(h_f))
+        elif self._features == 'sigmoid':
+            h_f = F.sigmoid(self._h4(h_f))
         else:
-            h = F.relu(self._h2(h))
-            h = F.relu(self._h3(h))
-            h_f = h.view(-1, 3136)
+            raise ValueError
 
-        features = list()
-        q = list()
-
-        for i in range(self._n_games):
-            features.append(F.relu(self._h4[i](h_f)))
-            q.append(self._h5[i](features[i]))
-
+        q = [self._h5[i](h_f) for i in range(self._n_games)]
         q = torch.stack(q, dim=1)
 
         if action is not None:
@@ -107,6 +113,53 @@ class Network(nn.Module):
             return q, h_f
         else:
             return q
+
+    def get_shared_weights(self):
+        p2 = list()
+        p3 = list()
+        p4 = list()
+
+        for p in self._h2.parameters():
+            p2.append(p.data.detach().cpu().numpy())
+
+        for p in self._h3.parameters():
+            p3.append(p.data.detach().cpu().numpy())
+
+        for p in self._h4.parameters():
+            p4.append(p.data.detach().cpu().numpy())
+
+        return p2, p3, p4
+
+    def set_shared_weights(self, weights):
+        w2, w3, w4 = weights
+
+        for p, w in zip(self._h2.parameters(), w2):
+            w_tensor = torch.from_numpy(w).type(p.data.dtype)
+            p.data = w_tensor
+
+        for p, w in zip(self._h3.parameters(), w3):
+            w_tensor = torch.from_numpy(w).type(p.data.dtype)
+            p.data = w_tensor
+
+        for p, w in zip(self._h4.parameters(), w4):
+            w_tensor = torch.from_numpy(w).type(p.data.dtype)
+            p.data = w_tensor
+
+    def freeze_shared_weights(self):
+        for p in self._h2.parameters():
+            p.requires_grad = False
+        for p in self._h3.parameters():
+            p.requires_grad = False
+        for p in self._h4.parameters():
+            p.requires_grad = False
+
+    def unfreeze_shared_weights(self):
+        for p in self._h2.parameters():
+            p.requires_grad = True
+        for p in self._h3.parameters():
+            p.requires_grad = True
+        for p in self._h4.parameters():
+            p.requires_grad = True
 
 
 def print_epoch(epoch):
@@ -166,9 +219,10 @@ def experiment():
     arg_net.add_argument("--reg-coeff", type=float, default=0.)
 
     arg_alg = parser.add_argument_group('Algorithm')
+    arg_alg.add_argument("--algorithm", default='dqn', choices=['dqn', 'ddqn'])
+    arg_alg.add_argument("--features", choices=['relu', 'sigmoid'],
+                         default='sigmoid')
     arg_alg.add_argument("--dropout", action='store_true')
-    arg_alg.add_argument("--distill", action='store_true')
-    arg_alg.add_argument("--entropy-coeff", type=float, default=np.inf)
     arg_alg.add_argument("--batch-size", type=int, default=32,
                          help='Batch size for each fit of the network.')
     arg_alg.add_argument("--history-length", type=int, default=4,
@@ -201,6 +255,13 @@ def experiment():
     arg_alg.add_argument("--max-no-op-actions", type=int, default=30,
                          help='Maximum number of no-op actions performed at the'
                               'beginning of the episodes.')
+    arg_alg.add_argument("--transfer", type=str, default='',
+                         help='Path to  the file of the weights of the common '
+                              'layers to be loaded')
+    arg_alg.add_argument("--save-shared", type=str, default='',
+                         help='filename where to save the shared weights')
+    arg_alg.add_argument("--unfreeze-epoch", type=int, default=0,
+                         help="Number of epoch where to unfreeze shared weights.")
 
     arg_utils = parser.add_argument_group('Utils')
     arg_utils.add_argument('--use-cuda', action='store_true',
@@ -222,11 +283,23 @@ def experiment():
 
     args.games = [''.join(g) for g in args.games]
 
+    losses = list()
+    l1_losses = list()
     def regularized_loss(arg, y):
         yhat, h_f = arg
-        loss = F.smooth_l1_loss(yhat, y) * len(args.games)
 
-        return loss + args.reg_coeff * torch.norm(h_f, 1)
+        loss = F.smooth_l1_loss(yhat, y, reduce=False)
+        temp_losses = list()
+        for i in range(len(args.games)):
+            start = i * args.batch_size
+            stop = start + args.batch_size
+            temp_losses.append(torch.mean(loss[start:stop]).item())
+        losses.append(temp_losses)
+        loss = torch.mean(loss)
+        l1_loss = torch.norm(h_f, 1) / h_f.shape[0]
+        l1_losses.append(l1_loss.item())
+
+        return loss + args.reg_coeff * l1_loss
 
     scores = list()
     for _ in range(len(args.games)):
@@ -273,9 +346,10 @@ def experiment():
         if m > max_act_n:
             max_act_n = m
             max_act_idx = i
+    gammas = [m.info.gamma for m in mdp]
+    horizons = [m.info.horizon for m in mdp]
     mdp_info = MDPInfo(mdp[max_obs_idx].info.observation_space,
-                       mdp[max_act_idx].info.action_space, mdp[0].info.gamma,
-                       mdp[0].info.horizon)
+                       mdp[max_act_idx].info.action_space, gammas, horizons)
 
     # Evaluation of the model provided by the user.
     if args.load_path:
@@ -297,7 +371,8 @@ def experiment():
             optimizer=optimizer,
             loss=regularized_loss,
             use_cuda=args.use_cuda,
-            dropout=args.dropout
+            dropout=args.dropout,
+            features=args.features
         )
 
         approximator = PyTorchApproximator
@@ -311,9 +386,7 @@ def experiment():
             initial_replay_size=0,
             max_replay_size=0,
             history_length=args.history_length,
-            dtype=np.uint8,
-            distill=args.distill,
-            entropy_coeff=args.entropy_coeff
+            dtype=np.uint8
         )
         agent = DQN(approximator, pi, mdp_info,
                     approximator_params=approximator_params, **algorithm_params)
@@ -337,7 +410,7 @@ def experiment():
 
         # Settings
         if args.debug:
-            initial_replay_size = 50
+            initial_replay_size = args.batch_size
             max_replay_size = 500
             train_frequency = 5
             target_update_frequency = 10
@@ -374,7 +447,8 @@ def experiment():
             optimizer=optimizer,
             loss=regularized_loss,
             use_cuda=args.use_cuda,
-            dropout=args.dropout
+            dropout=args.dropout,
+            features=args.features
         )
 
         approximator = PyTorchApproximator
@@ -388,14 +462,19 @@ def experiment():
             target_update_frequency=target_update_frequency // train_frequency,
             n_actions_per_head=n_actions_per_head,
             history_length=args.history_length,
-            dtype=np.uint8,
-            distill=args.distill,
-            entropy_coeff=args.entropy_coeff
+            dtype=np.uint8
         )
 
-        agent = DQN(approximator, pi, mdp_info,
-                    approximator_params=approximator_params,
-                    **algorithm_params)
+        if args.algorithm == 'dqn':
+            agent = DQN(approximator, pi, mdp_info,
+                        approximator_params=approximator_params,
+                        **algorithm_params)
+        elif args.algorithm == 'ddqn':
+            agent = DoubleDQN(approximator, pi, mdp_info,
+                              approximator_params=approximator_params,
+                              **algorithm_params)
+        else:
+            raise ValueError
 
         # Algorithm
         core = Core(agent, mdp)
@@ -411,6 +490,10 @@ def experiment():
         if args.save:
             agent.approximator.model.save()
 
+        if args.transfer:
+            weights = pickle.load(open(args.transfer, 'rb'))
+            agent.set_shared_weights(weights)
+
         for m in mdp:
             m.set_episode_end(False)
         # Evaluate initial policy
@@ -421,8 +504,17 @@ def experiment():
             d = dataset[i::len(mdp)]
             scores[i].append(get_stats(d, i, args.games))
 
+        if args.unfreeze_epoch > 0:
+            agent.freeze_shared_weights()
+
+        best_score_sum = -np.inf
+        best_weights = None
+
         np.save(folder_name + '/scores.npy', scores)
         for n_epoch in range(1, max_steps // evaluation_frequency + 1):
+            if n_epoch >= args.unfreeze_epoch > 0:
+                agent.unfreeze_shared_weights()
+
             print_epoch(n_epoch)
             print('- Learning:')
             # learning step
@@ -442,11 +534,23 @@ def experiment():
             pi.set_epsilon(epsilon_test)
             dataset = core.evaluate(n_steps=test_samples,
                                     render=args.render, quiet=args.quiet)
+
+            current_score_sum = 0
             for i in range(len(mdp)):
                 d = dataset[i::len(mdp)]
-                scores[i].append(get_stats(d, i, args.games))
+                current_score = get_stats(d, i, args.games)
+                scores[i].append(current_score)
+                current_score_sum += current_score[2]
+
+            # Save shared weights if best score
+            if args.save_shared and current_score_sum >= best_score_sum:
+                best_score_sum = current_score_sum
+                best_weights = agent.get_shared_weights()
 
             np.save(folder_name + '/scores.npy', scores)
+
+    if args.save_shared:
+        pickle.dump(best_weights, open(args.save_shared, 'wb'))
 
     return scores
 
