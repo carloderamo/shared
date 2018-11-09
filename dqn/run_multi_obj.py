7481000e4ba4a8f5ed929a3ffcfd392672655222
carloderamo <carlo.deramo@gmail.com>
2018-11-09 16:33:05 +0100

    fix

--- dqn/run_multi_obj.py
+++ dqn/run_multi_obj.py
@@ -99,12 +99,12 @@ def experiment():
                               'neural network.')
     arg_alg.add_argument("--max-steps", type=int, default=50000,
                          help='Total number of learning steps.')
-    arg_alg.add_argument("--final-exploration-frame", type=int, default=5000,
+    arg_alg.add_argument("--final-exploration-frame", type=int, default=25000,
                          help='Number of steps until the exploration rate stops'
                               'decreasing.')
     arg_alg.add_argument("--initial-exploration-rate", type=float, default=1.,
                          help='Initial value of the exploration rate.')
-    arg_alg.add_argument("--final-exploration-rate", type=float, default=.01,
+    arg_alg.add_argument("--final-exploration-rate", type=float, default=.1,
                          help='Final value of the exploration rate. When it'
                               'reaches this values, it stays constant.')
     arg_alg.add_argument("--test-exploration-rate", type=float, default=0.,
