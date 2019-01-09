b20bfa4f00b50e8c011219cbd136b7d90bd9ba83
carloderamo <carloderamo@gmail.com>
2019-01-09 17:15:28 +0100

    imgs added

--- results/dqn/transfer_plot.py
+++ results/dqn/transfer_plot.py
@@ -17,35 +17,46 @@ game_ids = [1]
 reg = ['noreg']
 activation = ['sigmoid']
 n_games = len(games)
-unfreezes = [0, 10]
+unfreezes = [0, 10, 51]
 
 legend_items = list()
 
 fig, ax = plt.subplots(n_games, 1)
-for i, g in enumerate(games):
-    ax.set_title(g)
-    ax.grid()
+# for i, g in enumerate(games):
+#     ax.set_title(g)
+#     ax.grid()
 
 for act in activation:
     for r in reg:
-        legend_items.append('dqn-' + r + '-' + act)
+        legend_items.append('Single DQN')
         path = 'dqn/' + r + '-' + act + '/'
         a = np.load(path + 'scores.npy')
         a_mean, a_err = get_mean_and_confidence(a)
         for i, idx in enumerate(game_ids):
-            ax.plot(a_mean[idx])
+            ax.plot(a_mean[idx], linewidth=3)
             ax.fill_between(np.arange(51), a_mean[idx] - a_err[idx], a_mean[idx] + a_err[idx], alpha=.5)
     
     for u in unfreezes:
         for i, g in zip(game_ids, games):
-            legend_items.append('transfer' + '-unfreeze' + str(u) + '-' + r + '-' + act)
+            if u == 51:
+                legend_items.append('No unfreeze')
+            else:
+                legend_items.append('Unfreeze-' + str(u))
             file_path = 'transfer' + '/' + g + '/unfreeze' + str(u) + '-' + r + '-' + act + '.npy'
 
             a = np.load(file_path)
             a_mean, a_err = get_mean_and_confidence(a)
-            ax.plot(a_mean[0])
+            ax.plot(a_mean[0], linewidth=3)
             ax.fill_between(np.arange(51), a_mean[0] - a_err[0], a_mean[0] + a_err[0], alpha=.5)
 
-plt.legend(legend_items)
+plt.xlabel('#Epochs', fontsize='xx-large')
+plt.ylabel('Performance', fontsize='xx-large')
+plt.xticks(fontsize='xx-large')
+plt.yticks(fontsize='xx-large')
+
+plt.grid()
+
+plt.legend(legend_items, fontsize='xx-large', loc='lower right')
+
 plt.show()
     
