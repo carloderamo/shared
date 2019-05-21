016fe9b3fd852467ca15454d0a2f86532d527dca
carloderamo <carlo.deramo@gmail.com>
2019-05-21 11:30:38 +0200

    transfer plot ddpg udpated

--- results/ddpg/transfer_plot.py
+++ results/ddpg/transfer_plot.py
@@ -11,6 +11,18 @@ def get_mean_and_confidence(data):
     interval, _ = st.t.interval(0.95, n-1, scale=se)
 
     return mean, interval
+    
+show_pendulum = False
+leg_idx = 0 if show_pendulum else -1
+
+if show_pendulum:
+    alg = 'multi_pendulum'
+    game = 'InvertedDoublePendulumBulletEnv-v0'
+    title = 'Inverted-Double-Pendulum'
+else:
+    alg = 'multi_walker'
+    game = 'hop_stand'
+    title = 'Hopper'
 
 games = ['noreg']
 game_ids = [0]
@@ -29,8 +41,8 @@ fig, ax = plt.subplots(n_games, 1)
 for act in activation:
     for r in reg:
         legend_items.append('No initialization')
-        path = 'single/' + r + '-' + act + '/InvertedDoublePendulumBulletEnv-v0/'
-        a = np.load(path + 'scores.npy')
+        path = 'single/' + r + '-' + act + '/' + game
+        a = np.load(path + '.npy')
         a_mean, a_err = get_mean_and_confidence(a)
         for i, idx in enumerate(game_ids):
             ax.plot(a_mean[idx], linewidth=3)
@@ -42,7 +54,7 @@ for act in activation:
                 legend_items.append('No unfreeze')
             else:
                 legend_items.append('Unfreeze-' + str(u))
-            file_path = 'multi_pendulum/transfer' + '/' + g + '/unfreeze' + str(u) + '-' + r + '-' + act + '.npy'
+            file_path = alg + '/transfer' + '/' + g + '/unfreeze' + str(u) + '-' + r + '-' + act + '.npy'
 
             a = np.load(file_path)
             a_mean, a_err = get_mean_and_confidence(a)
@@ -56,7 +68,7 @@ plt.yticks(fontsize=35)
 
 plt.grid()
 
-plt.title('Inverted-Double-Pendulum', fontsize=35)
+plt.title(title, fontsize=35)
 
 plt.legend(legend_items, fontsize=35, bbox_to_anchor=[.35, .7])
 
