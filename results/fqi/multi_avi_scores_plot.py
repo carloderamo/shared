8ef3fa6b984f2fbca47ecd64f72c73104f9ff10d
carloderamo <carlo.deramo@gmail.com>
2019-09-17 13:54:07 +0200

    plotter updated

--- results/fqi/multi_avi_scores_plot.py
+++ results/fqi/multi_avi_scores_plot.py
@@ -2,6 +2,7 @@ import numpy as np
 import scipy.stats as st
 from matplotlib import pyplot as plt
 from mpl_toolkits.axes_grid1.inset_locator import zoomed_inset_axes
+from mpl_toolkits.axes_grid1.inset_locator import mark_inset
 
 
 def get_mean_and_confidence(data):
@@ -31,13 +32,14 @@ plt.grid()
 plt.legend(n_tasks, fontsize='xx-large')
 
 axins = zoomed_inset_axes(ax, 2, loc=9) # zoom-factor: 2.5, location: upper-left
+mark_inset(ax, axins, loc1=4, loc2=3, fc="none", ec="0.5")
 for i in n_tasks:
     a = np.load(''.join(games[:i]) + '/avi_diff.npy')
     
     a_mean, a_err = get_mean_and_confidence(a)
     axins.plot(a_mean, linewidth=3)
     axins.fill_between(np.arange(a_mean.shape[-1]), a_mean - a_err, a_mean + a_err, alpha=.5)
-    x1, x2, y1, y2 = 40, 50, .13, .22 # specify the limits
+    x1, x2, y1, y2 = 40, 49, .155, .225 # specify the limits
     axins.set_xlim(x1, x2) # apply the x-limits
     axins.set_ylim(y1, y2) # apply the y-limits
 axins.grid()
