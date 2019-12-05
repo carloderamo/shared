de92f24b55a6961a0e1781662362bafd9c7da96f
carloderamo <carlo.deramo@gmail.com>
2019-12-05 11:52:25 +0100

    atari multi added

--- dqn/networks.py
+++ dqn/networks.py
@@ -5,10 +5,124 @@ import torch.nn.functional as F
 import numpy as np
 
 
+class AtariNetwork(nn.Module):
+    n_features = 512
+
+    def __init__(self, input_shape, _, n_actions_per_head, use_cuda, n_games,
+                 features, dropout):
+        super().__init__()
+
+        self._n_input = input_shape
+        self._n_games = n_games
+        self._max_actions = max(n_actions_per_head)[0]
+        self._features = features
+        self._use_cuda = use_cuda
+        self._n_shared = 2
+
+        self._h1 = nn.ModuleList(
+            [nn.Conv2d(self._n_input[0], 32, kernel_size=8, stride=4) for _ in range(
+                self._n_games)]
+        )
+        self._h2 = nn.ModuleList(
+            [nn.Conv2d(32, 64, kernel_size=4, stride=2) for _ in range(
+                self._n_games)]
+        )
+        self._h3 = nn.ModuleList(
+            [nn.Conv2d(64, 64, kernel_size=3, stride=1) for _ in range(
+                self._n_games)]
+        )
+        self._h4 = nn.Linear(3136, self.n_features)
+        self._h5 = nn.ModuleList(
+            [nn.Linear(self.n_features, self._max_actions) for _ in range(
+                self._n_games)]
+        )
+
+        nn.init.xavier_uniform_(self._h4.weight,
+                                gain=nn.init.calculate_gain('relu'))
+        for i in range(self._n_games):
+            nn.init.xavier_uniform_(self._h1[i].weight,
+                                    gain=nn.init.calculate_gain('relu'))
+            nn.init.xavier_uniform_(self._h2[i].weight,
+                                    gain=nn.init.calculate_gain('relu'))
+            nn.init.xavier_uniform_(self._h3[i].weight,
+                                    gain=nn.init.calculate_gain('relu'))
+            nn.init.xavier_uniform_(self._h5[i].weight,
+                                    gain=nn.init.calculate_gain('linear'))
+
+    def forward(self, state, action=None, idx=None):
+        state = state.float()
+
+        h = list()
+        for i in np.unique(idx):
+            idxs = np.argwhere(idx == i).ravel()
+            h_f = F.relu(
+                self._h1[i](state[idxs, :self._n_input[0]].float() / 255.)
+            )
+            h_f = F.relu(self._h2[i](h_f))
+            h.append(F.relu(self._h3[i](h_f)))
+        cat_h3 = torch.cat(h)
+
+        if self._features == 'relu':
+            h_f = F.relu(self._h4(cat_h3.view(-1, 3136)))
+        elif self._features == 'sigmoid':
+            h_f = torch.sigmoid(self._h4(cat_h3.view(-1, 3136)))
+        else:
+            raise ValueError
+
+        q = [self._h5[i](h_f) for i in range(self._n_games)]
+        q = torch.stack(q, dim=1)
+
+        if action is not None:
+            action = action.long()
+            q_acted = torch.squeeze(
+                q.gather(2, action.repeat(1, self._n_games).unsqueeze(-1)), -1)
+
+            q = q_acted
+
+        if idx is not None:
+            idx = torch.from_numpy(idx)
+            if self._use_cuda:
+                idx = idx.cuda()
+            if q.dim() == 2:
+                q_idx = q.gather(1, idx.unsqueeze(-1))
+            else:
+                q_idx = q.gather(1, idx.view(-1, 1).repeat(
+                    1, self._max_actions).unsqueeze(1))
+
+            q = torch.squeeze(q_idx, 1)
+
+        return q
+
+    def get_shared_weights(self):
+        p1 = list()
+
+        for p in self._h4.parameters():
+            p1.append(p.data.detach().cpu().numpy())
+
+        return p1
+
+    def set_shared_weights(self, weights):
+        w1 = weights
+
+        for p, w in zip(self._h4.parameters(), w1):
+            w_tensor = torch.from_numpy(w).type(p.data.dtype)
+            if self._use_cuda:
+                w_tensor = w_tensor.cuda()
+            p.data = w_tensor
+
+    def freeze_shared_weights(self):
+        for p in self._h4.parameters():
+            p.requires_grad = False
+
+    def unfreeze_shared_weights(self):
+        for p in self._h4.parameters():
+            p.requires_grad = True
+
+
 class GymNetwork(nn.Module):
     def __init__(self, input_shape, _, n_actions_per_head, use_cuda, features,
                  dropout, n_features=80):
-        super(GymNetwork, self).__init__()
+        super().__init__()
 
         self._n_input = input_shape
         self._n_games = len(n_actions_per_head)
---
+++ dqn/run_atari.py
@@ -0,0 +1,376 @@
+import argparse
+import datetime
+import pathlib
+import sys
+
+from joblib import delayed, Parallel
+import numpy as np
+import torch.optim as optim
+
+import pickle
+
+sys.path.append('..')
+
+from mushroom.approximators.parametric.torch_approximator import TorchApproximator
+from mushroom.environments import *
+from mushroom.utils.dataset import compute_J
+from mushroom.utils.parameters import LinearParameter, Parameter
+
+from core import Core
+from dqn import DQN
+from policy import EpsGreedyMultiple
+from networks import AtariNetwork
+from losses import LossFunction
+from replay_memory import PrioritizedReplayMemory
+
+"""
+This script runs Atari experiments with DQN as presented in:
+"Human-Level Control Through Deep Reinforcement Learning". Mnih V. et al.. 2015.
+
+"""
+
+
+def print_epoch(epoch):
+    print('################################################################')
+    print('Epoch: ', epoch)
+    print('----------------------------------------------------------------')
+
+
+def get_stats(dataset, gamma, idx, games):
+    J = np.mean(compute_J(dataset, gamma[idx]))
+    print(games[idx] + ': J: %f' % J)
+
+    return J
+
+
+def experiment(args, idx):
+    np.random.seed()
+
+    args.games = [''.join(g) for g in args.games]
+
+    # MDP
+    mdp = list()
+    gamma_eval = list()
+    for i, g in enumerate(args.games):
+        mdp.append(Atari(g))
+        gamma_eval.append(mdp[0].info.gamma)
+
+    n_actions_per_head = [(m.info.action_space.n,) for m in mdp]
+
+    max_obs_dim = 0
+    max_act_n = 0
+    for i in range(len(args.games)):
+        n = mdp[i].info.observation_space.shape[0]
+        m = mdp[i].info.action_space.n
+        if n > max_obs_dim:
+            max_obs_dim = n
+            max_obs_idx = i
+        if m > max_act_n:
+            max_act_n = m
+            max_act_idx = i
+    gammas = [m.info.gamma for m in mdp]
+    horizons = [m.info.horizon for m in mdp]
+    mdp_info = MDPInfo(mdp[max_obs_idx].info.observation_space,
+                       mdp[max_act_idx].info.action_space, gammas, horizons)
+
+    scores = list()
+    for _ in range(len(args.games)):
+        scores.append(list())
+
+    optimizer = dict()
+    if args.optimizer == 'adam':
+        optimizer['class'] = optim.Adam
+        optimizer['params'] = dict(lr=args.learning_rate,
+                                   eps=args.epsilon)
+    elif args.optimizer == 'adadelta':
+        optimizer['class'] = optim.Adadelta
+        optimizer['params'] = dict(lr=args.learning_rate,
+                                   eps=args.epsilon)
+    elif args.optimizer == 'rmsprop':
+        optimizer['class'] = optim.RMSprop
+        optimizer['params'] = dict(lr=args.learning_rate,
+                                   alpha=args.decay,
+                                   eps=args.epsilon)
+    elif args.optimizer == 'rmspropcentered':
+        optimizer['class'] = optim.RMSprop
+        optimizer['params'] = dict(lr=args.learning_rate,
+                                   alpha=args.decay,
+                                   eps=args.epsilon,
+                                   centered=True)
+    else:
+        raise ValueError
+
+    # DQN learning run
+
+    # Settings
+    if args.debug:
+        initial_replay_size = args.batch_size
+        max_replay_size = 500
+        train_frequency = 5
+        target_update_frequency = 10
+        test_samples = 20
+        evaluation_frequency = 50
+        max_steps = 1000
+    else:
+        initial_replay_size = args.initial_replay_size
+        max_replay_size = args.max_replay_size
+        train_frequency = args.train_frequency
+        target_update_frequency = args.target_update_frequency
+        test_samples = args.test_samples
+        evaluation_frequency = args.evaluation_frequency
+        max_steps = args.max_steps
+
+    # Policy
+    epsilon = LinearParameter(value=args.initial_exploration_rate,
+                              threshold_value=args.final_exploration_rate,
+                              n=args.final_exploration_frame)
+    epsilon_test = Parameter(value=args.test_exploration_rate)
+    epsilon_random = Parameter(value=1)
+    pi = EpsGreedyMultiple(parameter=epsilon,
+                           n_actions_per_head=n_actions_per_head)
+
+    # Approximator
+    input_shape = [m.info.observation_space.shape for m in mdp]
+    n_games = len(args.games)
+    loss = LossFunction(n_games, args.batch_size,
+                        args.evaluation_frequency)
+
+    input_shape = (args.history_length, args.screen_height,
+                   args.screen_width)
+    approximator_params = dict(
+        network=AtariNetwork,
+        input_shape=input_shape,
+        output_shape=(max(n_actions_per_head)[0],),
+        n_actions=max(n_actions_per_head)[0],
+        n_actions_per_head=n_actions_per_head,
+        n_games=len(args.games),
+        optimizer=optimizer,
+        loss=loss,
+        use_cuda=args.use_cuda,
+        features=args.features
+    )
+
+    approximator = TorchApproximator
+
+    if args.prioritized:
+        replay_memory = [PrioritizedReplayMemory(
+            initial_replay_size, max_replay_size, alpha=.6,
+            beta=LinearParameter(.4, threshold_value=1,
+                                 n=max_steps // train_frequency)
+        ) for _ in range(n_games)]
+    else:
+        replay_memory = None
+
+    # Agent
+    algorithm_params = dict(
+        batch_size=args.batch_size,
+        n_games=len(args.games),
+        initial_replay_size=initial_replay_size,
+        max_replay_size=max_replay_size,
+        target_update_frequency=target_update_frequency // train_frequency,
+        replay_memory=replay_memory,
+        n_actions_per_head=n_actions_per_head,
+        clip_reward=True,
+        history_length=args.history_length
+    )
+
+    agent = DQN(approximator, pi, mdp_info,
+                approximator_params=approximator_params,
+                **algorithm_params)
+
+    # Algorithm
+    core = Core(agent, mdp)
+
+    # RUN
+
+    # Fill replay memory with random dataset
+    print_epoch(0)
+    pi.set_parameter(epsilon_random)
+    core.learn(n_steps=initial_replay_size,
+               n_steps_per_fit=initial_replay_size, quiet=args.quiet)
+
+    if args.transfer:
+        weights = pickle.load(open(args.transfer, 'rb'))
+        agent.set_shared_weights(weights)
+
+    if args.load:
+        weights = np.load(args.load)
+        agent.approximator.set_weights(weights)
+
+    # Evaluate initial policy
+    pi.set_parameter(epsilon_test)
+    dataset = core.evaluate(n_steps=test_samples, render=args.render,
+                            quiet=args.quiet)
+    for i in range(len(mdp)):
+        d = dataset[i::len(mdp)]
+        scores[i].append(get_stats(d, gamma_eval, i, args.games))
+
+    if args.unfreeze_epoch > 0:
+        agent.freeze_shared_weights()
+
+    best_score_sum = -np.inf
+    best_weights = None
+
+    np.save(folder_name + 'scores-exp-%d.npy' % idx, scores)
+    np.save(folder_name + 'loss-exp-%d.npy' % idx,
+            agent.approximator.model._loss.get_losses())
+
+    for n_epoch in range(1, max_steps // evaluation_frequency + 1):
+        if n_epoch >= args.unfreeze_epoch > 0:
+            agent.unfreeze_shared_weights()
+
+        print_epoch(n_epoch)
+        print('- Learning:')
+        # learning step
+        pi.set_parameter(None)
+        core.learn(n_steps=evaluation_frequency,
+                   n_steps_per_fit=train_frequency, quiet=args.quiet)
+
+        print('- Evaluation:')
+        # evaluation step
+        pi.set_parameter(epsilon_test)
+        dataset = core.evaluate(n_steps=test_samples,
+                                render=args.render, quiet=args.quiet)
+
+        current_score_sum = 0
+        for i in range(len(mdp)):
+            d = dataset[i::len(mdp)]
+            current_score = get_stats(d, gamma_eval, i, args.games)
+            scores[i].append(current_score)
+            current_score_sum += current_score
+
+        # Save shared weights if best score
+        if args.save_shared and current_score_sum >= best_score_sum:
+            best_score_sum = current_score_sum
+            best_weights = agent.get_shared_weights()
+
+        if args.save:
+            np.save(folder_name + 'weights-exp-%d-%d.npy' % (idx, n_epoch),
+                    agent.approximator.get_weights())
+
+        np.save(folder_name + 'scores-exp-%d.npy' % idx, scores)
+        np.save(folder_name + 'loss-exp-%d.npy' % idx,
+                agent.approximator.model._loss.get_losses())
+
+    if args.save_shared:
+        pickle.dump(best_weights, open(args.save_shared, 'wb'))
+
+    return scores, agent.approximator.model._loss.get_losses()
+
+
+if __name__ == '__main__':
+    # Argument parser
+    parser = argparse.ArgumentParser()
+
+    arg_game = parser.add_argument_group('Game')
+    arg_game.add_argument("--games",
+                          type=list,
+                          nargs='+',
+                          default=['BreakoutNoFrameskip-v4'],
+                          help='Gym ID of the problem.')
+    arg_game.add_argument("--screen-width", type=int, default=84,
+                          help='Width of the game screen.')
+    arg_game.add_argument("--screen-height", type=int, default=84,
+                          help='Height of the game screen.')
+    arg_game.add_argument("--n-exp", type=int)
+
+    arg_mem = parser.add_argument_group('Replay Memory')
+    arg_mem.add_argument("--initial-replay-size", type=int, default=50000,
+                         help='Initial size of the replay memory.')
+    arg_mem.add_argument("--max-replay-size", type=int, default=500000,
+                         help='Max size of the replay memory.')
+    arg_mem.add_argument("--prioritized", action='store_true',
+                         help='Whether to use prioritized memory or not.')
+
+    arg_net = parser.add_argument_group('Deep Q-Network')
+    arg_net.add_argument("--optimizer",
+                         choices=['adadelta',
+                                  'adam',
+                                  'rmsprop',
+                                  'rmspropcentered'],
+                         default='adam',
+                         help='Name of the optimizer to use to learn.')
+    arg_net.add_argument("--learning-rate", type=float, default=.00025,
+                         help='Learning rate value of the optimizer. Only used'
+                              'in rmspropcentered')
+    arg_net.add_argument("--decay", type=float, default=.95,
+                         help='Discount factor for the history coming from the'
+                              'gradient momentum in rmspropcentered')
+    arg_net.add_argument("--epsilon", type=float, default=1e-8,
+                         help='Epsilon term used in rmspropcentered')
+
+    arg_alg = parser.add_argument_group('Algorithm')
+    arg_alg.add_argument("--features", choices=['relu', 'sigmoid'])
+    arg_alg.add_argument("--batch-size", type=int, default=32,
+                         help='Batch size for each fit of the network.')
+    arg_alg.add_argument("--history-length", type=int, default=4,
+                         help='Number of frames composing a state.')
+    arg_alg.add_argument("--target-update-frequency", type=int, default=10000,
+                         help='Number of collected samples before each update'
+                              'of the target network.')
+    arg_alg.add_argument("--evaluation-frequency", type=int, default=250000,
+                         help='Number of learning step before each evaluation.'
+                              'This number represents an epoch.')
+    arg_alg.add_argument("--train-frequency", type=int, default=4,
+                         help='Number of learning steps before each fit of the'
+                              'neural network.')
+    arg_alg.add_argument("--max-steps", type=int, default=50000000,
+                         help='Total number of learning steps.')
+    arg_alg.add_argument("--final-exploration-frame", type=int, default=1000000,
+                         help='Number of steps until the exploration rate stops'
+                              'decreasing.')
+    arg_alg.add_argument("--initial-exploration-rate", type=float, default=1.,
+                         help='Initial value of the exploration rate.')
+    arg_alg.add_argument("--final-exploration-rate", type=float, default=.1,
+                         help='Final value of the exploration rate. When it'
+                              'reaches this values, it stays constant.')
+    arg_alg.add_argument("--test-exploration-rate", type=float, default=.05,
+                         help='Exploration rate used during evaluation.')
+    arg_alg.add_argument("--test-samples", type=int, default=125000,
+                         help='Number of steps for each evaluation.')
+    arg_alg.add_argument("--max-no-op-actions", type=int, default=30,
+                         help='Maximum number of no-op action performed at the'
+                              'beginning of the episodes. The minimum number is'
+                              'history_length.')
+    arg_alg.add_argument("--transfer", type=str, default='',
+                         help='Path to  the file of the weights of the common '
+                              'layers to be loaded')
+    arg_alg.add_argument("--save-shared", type=str, default='',
+                         help='filename where to save the shared weights')
+    arg_alg.add_argument("--unfreeze-epoch", type=int, default=0,
+                         help="Number of epoch where to unfreeze shared weights.")
+
+    arg_utils = parser.add_argument_group('Utils')
+    arg_utils.add_argument('--use-cuda', action='store_true',
+                           help='Flag specifying whether to use the GPU.')
+    arg_utils.add_argument('--load', type=str,
+                           help='Path of the model to be loaded.')
+    arg_utils.add_argument('--save', action='store_true',
+                           help='Flag specifying whether to save the model.')
+    arg_utils.add_argument('--render', action='store_true',
+                           help='Flag specifying whether to render the game.')
+    arg_utils.add_argument('--quiet', action='store_true',
+                           help='Flag specifying whether to hide the progress'
+                                'bar.')
+    arg_utils.add_argument('--debug', action='store_true',
+                           help='Flag specifying whether the script has to be'
+                                'run in debug mode.')
+    arg_utils.add_argument('--postfix', type=str, default='',
+                           help='Flag used to add a postfix to the folder name')
+
+    args = parser.parse_args()
+
+    folder_name = './logs/gym_' + datetime.datetime.now().strftime(
+        '%Y-%m-%d_%H-%M-%S') + args.postfix + '/'
+    pathlib.Path(folder_name).mkdir(parents=True)
+    with open(folder_name + 'args.pkl', 'wb') as f:
+        pickle.dump(args, f)
+
+    out = Parallel(n_jobs=-1)(delayed(experiment)(args, i)
+                              for i in range(args.n_exp))
+
+    scores = np.array([o[0] for o in out])
+    loss = np.array([o[1] for o in out])
+
+    np.save(folder_name + 'scores.npy', scores)
+    np.save(folder_name + 'loss.npy', loss)
